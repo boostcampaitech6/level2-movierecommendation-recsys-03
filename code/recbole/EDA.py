d952ef9e475fc04de472ee8d88323abbdd616808
Jang <8chris8@naver.com>
2024-02-20 12:40:08 +0000

    feat:EDA

--- code/recbole/EDA.py
+++ code/recbole/EDA.py
@@ -4,13 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import re
 
-
-data_path = '/data/data/train'
-interactions = pd.read_csv(os.path.join(data_path, 'train_ratings.csv'))
-grouped_items = interactions.groupby('user')['item'].apply(list)
-
-
-data_path = '/data/data/train'
+data_path = 'data/train'
 interactions = pd.read_csv(os.path.join(data_path, 'train_ratings.csv'))
 directors = pd.read_csv(os.path.join(data_path, 'directors.tsv'), sep='\t')
 genres = pd.read_csv(os.path.join(data_path, 'genres.tsv'), sep='\t')
@@ -18,38 +12,199 @@ titles = pd.read_csv(os.path.join(data_path, 'titles.tsv'), sep='\t')
 writers = pd.read_csv(os.path.join(data_path, 'writers.tsv'), sep='\t')
 years = pd.read_csv(os.path.join(data_path, 'years.tsv'), sep='\t')
 
+
+## items 파일
+
 #Genre, Writer, Director 한칸에 넣기
 grouped_genres= genres.groupby('item')['genre'].apply(list)
 grouped_writers = writers.groupby('item')['writer'].apply(list)
 grouped_directors = directors.groupby('item')['director'].apply(list)
-
 items = pd.merge(titles, years, on='item')
 items = pd.merge(items, grouped_genres, on='item', how = 'left')
 items = pd.merge(items, grouped_writers, on='item', how = 'left')
 items = pd.merge(items, grouped_directors, on='item', how = 'left')
-
-#기존
-items = pd.merge(titles, years, on='item')
-items = pd.merge(items, genres, on='item')
-items = pd.merge(items, writers, on='item', how = 'left')
-items = pd.merge(items, directors, on='item', how = 'left')
-
 # year_bin 추가
-bins = pd.cut(items['year'], bins=10)
-bin_labels = [int(b.left) for b in bins.cat.categories]
-items['year_bin'] = pd.cut(items['year'], bins=10, labels=bin_labels)
-
+year_counts = interactions.merge(items[['item','year']], on='item',how='left')['year'].value_counts()
+year_counts = pd.DataFrame(year_counts)
+year_counts.reset_index(inplace=True)
+year_counts.columns = ['year', 'count']
+year_counts['cumulative_count'] = year_counts['count'].cumsum()
+labels = [f'year_{i}' for i in range(3,0,-1)]  # 10개 구간 레이블
+year_counts['year_bin'] = pd.cut(year_counts['cumulative_count'], bins=3, labels=labels)
+items = items.merge(year_counts[['year', 'year_bin']], on='year', how='left')
 # popularity_bin 추가
 item_counts = interactions['item'].value_counts()
 item_counts = pd.DataFrame(item_counts)
 item_counts.reset_index(inplace=True)
 item_counts.columns = ['item', 'count']
-item_counts['popularity_bin'] = pd.cut(item_counts['count'], bins=10, labels=False)
+#item count 기준
+item_counts['cumulative_count'] = item_counts['count'].cumsum()
+labels = [f'pop_{i}' for i in range(3,0,-1)]  # 10개 구간 레이블
+item_counts['popularity_bin'] = pd.cut(item_counts['cumulative_count'], bins=3, labels=labels)
 items = items.merge(item_counts[['item', 'popularity_bin']], on='item', how='left')
 
 
 
 
+## Users 파일
+
+#item 추가
+Users = interactions.groupby('user')['item'].agg(list).reset_index()
+Users['item_count'] = Users['item'].apply(len)
+merged_interactions = interactions.merge(items, on='item', how='left')
+#popularity 추가
+popularity_filled = merged_interactions.groupby(['user', 'popularity_bin']).size().unstack(fill_value=0)
+new_column_names = {pop_bin: f'pop_{int(pop_bin)}' for pop_bin in popularity_filled.columns}
+popularity_filled = popularity_filled.rename(columns=new_column_names)
+Users = pd.merge(Users, popularity_filled, on='user')
+#year 추가
+year_filled = merged_interactions.groupby(['user', 'year_bin']).size().unstack(fill_value=0)
+new_column_names = {year_bin: f'year_{int(year_bin)}' for year_bin in year_filled.columns}
+year_filled = year_filled.rename(columns=new_column_names)
+Users = pd.merge(Users, year_filled, on='user')
+#genre,writer,director 추가
+def process_genres(df, top_n=3, label='genre'):
+    rows = []  # 결과를 저장할 리스트 초기화
+    # 각 사용자별로 반복
+    for user, row in df.iterrows():
+        # 각 장르(또는 대체 용어)의 상호작용 횟수를 내림차순으로 정렬
+        sorted_df = row.sort_values(ascending=False)
+        # 상위 N개 장르와 해당 상호작용 횟수 추출
+        top_categories = sorted_df[:top_n].index.tolist()
+        top_counts = sorted_df[:top_n].values.tolist()
+        # 결과 딕셔너리 생성
+        row_dict = {'user': user}
+        for i in range(top_n):
+            row_dict[f'{label}_{i+1}'] = top_categories[i] if i < len(top_categories) else None
+            row_dict[f'{label}_{i+1}_count'] = top_counts[i] if i < len(top_counts) else 0
+        # 나머지 장르(또는 대체 용어)의 상호작용 횟수 합산
+        etc_count = sorted_df[top_n:].sum()
+        row_dict[f'{label}_etc'] = 'etc'
+        row_dict[f'{label}_etc_count'] = etc_count
+        # 결과 리스트에 추가
+        rows.append(row_dict)
+    # 결과 리스트를 DataFrame으로 변환
+    result_df = pd.DataFrame(rows)
+    result_df.set_index('user', inplace=True)
+    return result_df
+
+interactions_genres = interactions.merge(genres, on='item', how='left')
+genre_filled = interactions_genres.groupby(['user', 'genre']).size().unstack(fill_value=0)
+interactions_directors = interactions.merge(directors, on='item', how='left')
+director_filled = interactions_directors.groupby(['user', 'director']).size().unstack(fill_value=0)
+interactions_writers = interactions.merge(writers, on='item', how='left')
+writer_filled = interactions_writers.groupby(['user', 'writer']).size().unstack(fill_value=0)
+
+Users = pd.merge(Users, process_genres(genre_filled,label='genre'), on='user')
+Users = pd.merge(Users, process_genres(director_filled,label='director'), on='user')
+Users = pd.merge(Users, process_genres(writer_filled,label='writer'), on='user')
+
+# percent 추가
+for column in Users.columns:
+    if column in ['pop_3','pop_2','pop_1','year_1','year_2','year_3','genre_1_count','genre_2_count','genre_3_count'
+                      ,'director_1_count','director_2_count','director_3_count','writer_1_count','writer_2_count','writer_3_count']:
+        Users[column+'_percent'] = Users[column].div(Users['item_count'],axis=0)
+
+# type 추가 
+Users['type_popular'] = Users['pop_3_percent'] >= 0.7
+Users['type_unpopular'] = Users['pop_1_percent'] >= 0.6
+Users['type_new'] = Users['year_3_percent'] >= 0.7
+Users['type_old'] = Users['year_1_percent'] >= 0.6
+Users['type_genre'] = Users['genre_1_count_percent'] >= 0.7
+Users['type_director'] = Users['director_1_count_percent'] >= 0.08
+Users['type_writer'] = Users['writer_1_count_percent'] >= 0.08
+Users['type_mania'] = Users['item_count']>300
+Users['type_novice'] = Users['item_count']<50
+
+true_counts = {
+    'type_popular': Users['type_popular'].sum(),
+    'type_unpopular': Users['type_unpopular'].sum(),
+    'type_new': Users['type_new'].sum(),
+    'type_old': Users['type_old'].sum(),
+    'type_genre': Users['type_genre'].sum(),
+    'type_director': Users['type_director'].sum(),
+    'type_writer': Users['type_writer'].sum(),
+    'type_mania': Users['type_mania'].sum(),
+    'type_novice': Users['type_novice'].sum()
+}
+
+print(true_counts)
+
+Users['All_users'] = True
+
+Users['type'] = 'popular'
+Users['type'] = Users.apply(lambda Users: 'genre' if Users['type_genre'] else Users['type'], axis=1)
+Users['type'] = Users.apply(lambda Users: 'writer' if Users['type_writer'] else Users['type'], axis=1)
+Users['type'] = Users.apply(lambda Users: 'director' if Users['type_director'] else Users['type'], axis=1)
+Users['type'].value_counts()
+
+Users = Users.drop('item', axis=1)
+items.to_csv(os.path.join(data_path, 'EDA_items.csv'), sep=',', index=False)
+Users.to_csv(os.path.join(data_path, 'EDA_users.csv'), sep=',', index=False)
+
+
+
+## Submission 분석 #
+import os 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import re
+
+data_path = 'data/train'
+items = pd.read_csv(os.path.join(data_path, 'EDA_items.csv'), sep=',')
+Users = pd.read_csv(os.path.join(data_path, 'EDA_users.csv'), sep=',')
+
+output_path = 'code/recbole/output/'
+
+submission_name='slim'
+sub = pd.read_csv(output_path+'submission_'+submission_name+'.csv')
+sub = pd.merge(sub, items, on='item')
+
+import ast
+# 문자열을 리스트로 변환하는 함수 정의
+def convert_str_to_list(str_list):
+    try:
+        return ast.literal_eval(str_list)
+    except ValueError:  # 문자열이 올바른 리스트 형식이 아닐 경우
+        return []  # 빈 리스트 반환
+    end
+
+submission_EDA = pd.DataFrame({'user': Users['user'].copy()})
+
+
+
+year_bin_pivot = sub.pivot_table(index='user', columns='year_bin', aggfunc='size', fill_value=0)
+popularity_bin_pivot = sub.pivot_table(index='user', columns='popularity_bin', aggfunc='size', fill_value=0)
+
+submission_EDA = pd.merge(submission_EDA, year_bin_pivot, on='user')
+submission_EDA = pd.merge(submission_EDA, popularity_bin_pivot, on='user')
+
+def calculate_pivot(sub, Users, column_name, submission_EDA):
+    sub[column_name] = sub[column_name].apply(convert_str_to_list)
+    sub_exploded = sub.explode(column_name)
+    counts = sub_exploded.pivot_table(index='user', columns=column_name, aggfunc='size', fill_value=0)
+    counts_merge = pd.merge(counts, Users, on='user')
+    pivot_1 = counts_merge.apply(lambda row: row[row[column_name+'_1']] if row[column_name+'_1'] in counts_merge.columns else 0, axis=1)
+    submission_EDA[column_name+'_1']=pivot_1.values
+    pivot_2 = counts_merge.apply(lambda row: row[row[column_name+'_2']] if row[column_name+'_2'] in counts_merge.columns else 0, axis=1)
+    submission_EDA[column_name+'_2']=pivot_2.values
+    pivot_3 = counts_merge.apply(lambda row: row[row[column_name+'_3']] if row[column_name+'_3'] in counts_merge.columns else 0, axis=1)
+    submission_EDA[column_name+'_3']=pivot_3.values
+    return submission_EDA
+
+submission_EDA = calculate_pivot(sub,Users,'genre',submission_EDA)
+submission_EDA = calculate_pivot(sub,Users,'writer',submission_EDA)
+submission_EDA = calculate_pivot(sub,Users,'director',submission_EDA)
+
+submission_EDA = pd.merge(submission_EDA, Users[['user','type']], on='user')
+submission_EDA['model'] = submission_name
+submission_EDA.to_csv(os.path.join(output_path,'submission_'+submission_name+'_EDA.csv'), sep=',', index=False)
+
+
+## EDA ##
+
+#year, popularity PLOT
 item_counts_by_year_bin = items.groupby('year_bin').size()
 plt.bar(item_counts_by_year_bin.index, item_counts_by_year_bin.values)
 plt.savefig('year-item수.png')
--- code/recbole/rulebased.py
+++ code/recbole/rulebased.py
@@ -5,13 +5,11 @@ import pandas as pd
 data_path = '/data/data/train'
 interactions = pd.read_csv(os.path.join(data_path, 'train_ratings.csv'))
 genres = pd.read_csv(os.path.join(data_path, 'genres.tsv'), sep='\t')
-# interactions과 genres를 item을 기준으로 병합
-merged = interactions.merge(genres, on='item')
-# 사용자별로 본 장르를 리스트로 묶기
-user_genre_counts = merged.groupby('user')['genre'].value_counts()
-print(user_genre_counts)
-most_watched_genre_per_user = user_genre_counts.groupby(level=0).idxmax()
-print(most_watched_genre_per_user)
+writers = pd.read_csv(os.path.join(data_path, 'writers.tsv'), sep='\t')
+directors = pd.read_csv(os.path.join(data_path, 'directors.tsv'), sep='\t')
+
+
+Users = pd.read_csv(os.path.join(data_path, 'EDA_users.csv'), sep=',')
 
 
 user_item_group = interactions.groupby('user')['item'].apply(list)
@@ -22,23 +20,54 @@ print(item_counts)
 
 recommendations = []
 
-# 각 사용자에 대해서
+# 각 사용자와 그들의 시청 항목에 대해 반복
 for user, watched_items in user_item_group.items():
-    # 아직 시청하지 않은 아이템을 찾는다
+    # 시청하지 않은 항목을 찾음
     unwatched_items = item_counts[~item_counts.index.isin(watched_items)]
-    # 사용자가 가장 많이 시청한 장르의 아이템만 선택
-    genre_items = genres[genres['genre'] == most_watched_genre_per_user[user][1]]['item']
-    preferred_items = unwatched_items[unwatched_items.index.isin(genre_items)]
-    # 시청 횟수가 많은 순서대로 아이템을 정렬한다
+
+    # 사용자의 선호 타입을 확인
+    user_type = Users.loc[Users['user'] == user, 'type'].values[0]
+
+    if user_type == 'genre':
+        # 사용자의 가장 많이 시청한 장르에서 시청하지 않은 항목을 선택
+        genre_1 = Users.loc[Users['user'] == user, 'genre_1'].values[0]
+        genre_items = genres[genres['genre'] == genre_1]['item']
+        preferred_items = unwatched_items[unwatched_items.index.isin(genre_items)]
+    elif user_type == 'writer':
+        # 사용자의 가장 선호하는 작가의 작품 중 시청하지 않은 항목을 선택
+        preferred_writer = Users.loc[Users['user'] == user, 'writer_1'].values[0]
+        writer_items = writers[writers['writer'] == preferred_writer]['item']
+        preferred_items = unwatched_items[unwatched_items.index.isin(writer_items)]
+    elif user_type == 'director':
+        # 사용자의 가장 선호하는 감독의 작품 중 시청하지 않은 항목을 선택
+        preferred_director = Users.loc[Users['user'] == user, 'director_1'].values[0]
+        director_items = directors[directors['director'] == preferred_director]['item']
+        preferred_items = unwatched_items[unwatched_items.index.isin(director_items)]
+    else:
+        # 다른 타입의 경우, 사용자가 시청하지 않은 항목 중에서 선택
+        preferred_items = unwatched_items
+
+    # 시청 횟수에 따라 항목을 내림차순으로 정렬
     recommended_items = preferred_items.sort_values(ascending=False)
-    # 상위 10개 아이템을 추천한다
-    top_10_items = recommended_items[:10]
-    # 추천 결과를 저장한다
-    for item in top_10_items.index:
+
+    # 추천을 위한 상위 10개 항목을 선택
+    top_items = recommended_items[:10]
+
+    # preferred_items의 수가 10개 미만인 경우, 추가로 시청 횟수가 많은 항목들을 추천에 포함
+    if len(top_items) < 10:
+        extra_items_needed = 10 - len(top_items)
+        extra_items = unwatched_items[~unwatched_items.index.isin(top_items.index)].sort_values(ascending=False)[:extra_items_needed]
+        top_items = pd.concat([top_items, extra_items])
+
+    # 추천 결과를 저장
+    for item in top_items.index:
         recommendations.append((user, item))
+
 
 
 # 추천 결과를 DataFrame으로 변환한다
 recommendations_df = pd.DataFrame(recommendations, columns=['user', 'item'])
 print(recommendations_df)
-recommendations_df.to_csv('output/submission_rule_genres.csv',index=False)
+recommendations_df.to_csv('output/submission_rule_types2.csv',index=False)
+
+directors[directors['item']==3471]
