eba4400fd74be6420f5b0939b8816da6635bc0d9
Seungbin <pacificcatfich41@gmail.com>
2024-02-23 04:51:34 +0000

    feat: add weight and score

--- code/voting/voting.py
+++ code/voting/voting.py
@@ -8,13 +8,20 @@ import torch
 def main(args):
     
     outputs = list(map(lambda x: x.strip(), args.outputs.split(',')))
+    weights = list(map(lambda x: float(x.strip()), args.weights.split(',')))
     df = pd.DataFrame(columns = ['user','item'])
-    for output in outputs:
-        new_df = pd.read_csv(os.path.join(output),index_col=0,header=0)
+    for i, output in enumerate(outputs):
+        new_df = pd.read_csv(output,index_col=0,header=0)
+        new_df = new_df.groupby('user').head(30).reset_index()
+        score_list = list(map(lambda x: x*weights[i]*0.5, range(40, 10, -1)))
+        new_df['score'] = score_list * len(new_df['user'].unique())
         df = pd.concat([df, new_df], ignore_index=True)
 
-    count_df = df.groupby('user')['item'].value_counts().reset_index(name='count')
+    sum_df = df.groupby(['user', 'item']).sum().reset_index()
+    sum_df = sum_df.drop_duplicates(['user','item'])
+    count_df = sum_df.sort_values(by=['user', 'score'], ascending=[True, False])
     count_df = count_df.groupby('user').head(10).reset_index()
+
 
     output = pd.read_csv('../movie/data/eval/sample_submission.csv')
     output['item'] = count_df['item']
